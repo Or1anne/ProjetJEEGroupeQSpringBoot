4c8b27d08878706736415a3674d4dba9bdd5fa43
Minetriforce <jonathand.ngo.65@gmail.com>
2025-11-22 22:38:37 +0100

    Initialisation

---
+++ src/main/java/org/example/projetjeegroupeqspringboot/ProjetJeeGroupeQSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.projetjeegroupeqspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJeeGroupeQSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetJeeGroupeQSpringBootApplication.class, args);
+    }
+
+}
