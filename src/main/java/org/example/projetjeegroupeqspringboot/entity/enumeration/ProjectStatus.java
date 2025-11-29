90c4a9543e89ba65f61dc53f6bbc157c7c1a7ac9
Or1anne <133128206+Or1anne@users.noreply.github.com>
2025-11-29 21:10:54 +0100

    Modification de l'affichage des énumérations

--- src/main/java/org/example/projetjeegroupeqspringboot/entity/enumeration/ProjectStatus.java
+++ src/main/java/org/example/projetjeegroupeqspringboot/entity/enumeration/ProjectStatus.java
@@ -1,7 +1,17 @@
 package org.example.projetjeegroupeqspringboot.entity.enumeration;
 
 public enum ProjectStatus {
-    WORKED_ON,
-    FINISHED,
-    CANCELLED
+    WORKED_ON("En cours"),
+    FINISHED("Terminé"),
+    CANCELLED("Annulé");
+
+    private final String label;
+
+    ProjectStatus(String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
 }
